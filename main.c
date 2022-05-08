7e59ff0f4752642cd1054538caf5537ce888e8f0
lauren-safwat <lauren.safwat18@gmail.com>
2022-05-08 20:49:03 +0200

    Flow of program modified

--- main.c
+++ main.c
@@ -1,54 +1,16 @@
+/******************************************************************/
+/*   Authors     : Abdelrahman Tarek - Aya Hany - Lauren Safwat   */
+/*   Description : IMT Bank System                                */
+/*   Date        : 8 May 2022                                     */
+/*   Version     : V1.0                                           */
+/******************************************************************/
+
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-// Function to randomly generates password
-// of length N
-char randomPasswordGeneration(int N)
-{
-    // Initialize counter
-    int i = 0;
-    int randomizer = 0;
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-    // Array of numbers
-    char numbers[] = "0123456789";
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-    // Stores the random password
-    char password[N];
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-        randomizer = rand() % 4;
-    switch (randomizer) {
-    case 1:
-        password[i] = numbers[rand() % 10];
-        break;
-    case 2:
-        password[i] = symbols[rand() % 8];
-        break;
-    case 3:
-        password[i] = LETTER[rand() % 26];
-        break;
-    default:
-        password[i] = letter[rand() % 26];
-        break;
-    }
-    //return password;
-    }
-    printf("%s\n",password);
-    //return (char*)password;
-}
+#include <windows.h>
 
 
 typedef struct bank_account{
@@ -57,123 +19,136 @@ typedef struct bank_account{
     char national_id[15];  //national id must consist of 14 digit numbers
     int age;
     char bank_acc_id[11];
-    char gardian[40];
-    char gardian_nat_id[15];   // gardian national id must consist of 14 digits numbers
-    char status[10];
+    char guardian[40];
+    char guardian_nat_id[15];   // guardian national id must consist of 14 digits numbers
+    char status[11];
     float balance;
-    char password[6];  //Random Password will be generated for each account during creation of it
+    char *password;  //Random Password will be generated for each account during creation of it
+}Bank_Account;
 
-};
+
+
+
+/****************************************/
+/*         Functions Prototypes         */
+/****************************************/
+
+void gotoxy(int x, int y);
+void displayMainMenu();
+void displayAdminMainMenu();
+void displayClientMainMenu();
+char* randomPasswordGeneration(int N);
+void createNewAccount();
+
+
+
+
+/****************************************/
+/*            Main Function             */
+/****************************************/
 
 int main()
 {
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-//-----------------------------------------------------------------------
+    int window_choice, admin_choice, client_choice, choice;
+    Bank_Account acc;
+    char filename[15] , bank_account_id[11], national_id[15];
+    float amount;
+    FILE *fp, *fptr;
 
-//------------------------------------------------------------------------
+    home:
+        system("cls");
+        displayMainMenu();
+        scanf("%d", &window_choice);
+
+        main_menu:
+            system("cls");
+            gotoxy(55, 0);
+            printf("IMT Bank System");
+            gotoxy(55, 1);
+            printf("---------------");
+
+            switch(window_choice){
+            case 1:
+                displayAdminMainMenu();
+                scanf("%d", &admin_choice);
+
+                system("cls");
+                gotoxy(55, 0);
+                printf("IMT Bank System");
+                gotoxy(55, 1);
+                printf("---------------");
+
+                switch(admin_choice){
+                case 1:
+
+                    createNewAccount();
+                    printf("Press any key to return to the main menu...");
+                    getch();
+                    goto main_menu;
+                    break;
+
+                case 2:
+                    system("cls");
+                    printf("Press any key to return to the main menu...");
+                    getch();
+                    goto main_menu;
+                    break;
+
+                default:
+                    goto home;
+                }
+
+                break;
+
+            case 2:
+                displayClientMainMenu();
+                scanf("%d", &client_choice);
+
+                switch(client_choice) {
+                case 1:
+                    printf("Press any key to return to the main menu...");
+                    getch();
+                    goto main_menu;
+                    break;
+
+                case 2:
+                    printf("Press any key to return to the main menu...");
+                    getch();
+                    goto main_menu;
+                    break;
+
+                case 3:
+                    printf("Press any key to return to the main menu...");
+                    getch();
+                    goto main_menu;
+                    break;
+
+                case 4:
+                    printf("Press any key to return to the main menu...");
+                    getch();
+                    goto main_menu;
+                    break;
+
+                default:
+                    goto home;
+                }
+                break;
+
+            default:
+                exit(0);
+            }
+}
+
+
+int main_func()
+{
     int option,choice;
     struct bank_account acc;
     char filename[15] , bank_account_id[11], national_id[15];
     float amount;
     FILE *fp, *fptr;
-    // goto part
-    menu:
 
-    printf("Welcome to IMT Bank Admin page\n");
-    printf("1.Create a new account\n\n");
-    printf("2.Open Existing Account\n\n");
-    printf("3.Exit the System\n\n");
-    printf("Enter your choice\n");
-
-    scanf("%d",&option);
-
-    if (option == 1){
-        system("cls");
-        printf("\nEnter your full name ");
-        scanf("%s",acc.full_name);
-        printf("\nEnter your full Address ");
-        scanf("%s",acc.address);
-
-        fflush(stdin);   // clear standard input
-        printf("\nEnter your age ");
-        scanf("%d",&acc.age);
-
-        printf("\nEnter your National ID\n");
-        scanf("%s",acc.national_id);
-
-        if (acc.age < 21){
-            fflush(stdin);   //clear standard input
-            printf("\nEnter your Guardian National ID ");
-            scanf("%s",acc.gardian_nat_id);
-        }
-        printf("\nEnter your Balance ");
-        scanf("%f",&acc.balance);
-        printf("Bank account Id created randomly\n");
-        printf("Password created randomly\n");
-
-        stpcpy(acc.status,"Active");
-        // create filename based on national id just for now
-        stpcpy(filename, acc.national_id);  // todo change to bank account id
-        fp=fopen(strcat(filename,".dat"),"w");
-		fwrite(&acc,sizeof(acc),1,fp);
-            if(fwrite != 0){
-                printf("Registered Succesfully\n");
-            }
-
-    // Initialize counter
-/*------------------------------------------------------------
-        int randomizer = 0;
-
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-        srand((unsigned int)(time(NULL)));
-
-    // Array of numbers
-        char numbers[] = "0123456789";
-
-    // Array of small alphabets
-        char letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-    // Array of capital alphabets
-        char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-    // Array of all the special symbols
-        char symbols[] = "!@#$^&*?";
-
-    // Stores the random password
-        char password[4];
-
-    // To select the randomizer
-    // inside the loop
-        randomizer = rand() % 4;
-        fflush(stdin);
-        int i = 0;
-    // Iterate over the range [0, N]
-        for (i = 0; i < 4; i++) {
-            randomizer = rand() % 4;
-            switch (randomizer) {
-                case 1:
-                    password[i] = numbers[rand() % 10];
-                    break;
-                //case 2:
-                  //  password[i] = symbols[rand() % 8];
-                    //break;
-                case 3:
-                    password[i] = LETTER[rand() % 26];
-                    break;
-                default:
-                    password[i] = letter[rand() % 26];
-                    break;
-        }
-        //printf("%s\n",password);
-        }
-        printf("%s\n",password);
-------------------------------------------------------------------------------------*/
-    }
-    else if (option == 2){
+    if (option == 2){
         system("cls");
         printf("\nEnter Bank Account ID\n");
         //scanf("%s",bank_account_id);
@@ -212,9 +187,7 @@ int main()
 //                    }
 //                }
             }
-            else if (choice == 5){
-                    goto menu;
-    }
+
 	}
 
     }
@@ -223,4 +196,154 @@ int main()
     }
 
     return 0;
+}
+
+
+
+
+/****************************************/
+/*        Functions Definitions         */
+/****************************************/
+
+void gotoxy(int x, int y)
+{
+   COORD coord;
+   coord.X = x;
+   coord.Y = y;
+   SetConsoleCursorPosition(GetStdHandle ( STD_OUTPUT_HANDLE ), coord);
+}
+
+
+void displayMainMenu()
+{
+    gotoxy(50, 0);
+    printf("Welcome to IMT Bank System");
+    gotoxy(50, 1);
+    printf("--------------------------");
+    gotoxy(2, 3);
+    printf("1- Login as an admin");
+    gotoxy(2, 4);
+    printf("2- Login as a client");
+    gotoxy(2, 5);
+    printf("3- Exit the System");
+    gotoxy(2, 7);
+    printf("Enter your choice: ");
+}
+
+
+void displayAdminMainMenu()
+{
+    gotoxy(2, 3);
+    printf("- Admin Main Menu:");
+    gotoxy(2, 4);
+    printf("------------------");
+    gotoxy(4, 5);
+    printf("1- Create New Account");
+    gotoxy(4, 6);
+    printf("2- Open Existing Account");
+    gotoxy(4, 7);
+    printf("3- Log out");
+    gotoxy(4, 9);
+    printf("Enter your choice: ");
+}
+
+
+void displayClientMainMenu()
+{
+    gotoxy(2, 3);
+    printf("- Client Main Menu:");
+    gotoxy(2, 4);
+    printf("-------------------");
+    gotoxy(4, 5);
+    printf("1- Make Transaction");
+    gotoxy(4, 6);
+    printf("2- Change Account Password");
+    gotoxy(4, 7);
+    printf("3- Get Cash");
+    gotoxy(4, 8);
+    printf("4- Deposit in Account");
+    gotoxy(4, 9);
+    printf("5- Log out");
+    gotoxy(4, 11);
+    printf("Enter your choice: ");
+}
+
+
+/**********************************************************/
+
+// Function to randomly generate password of length N
+char* randomPasswordGeneration(int N)
+{
+    int i = 0;
+    int randomizer = 0;
+
+    // Seed the random-number generator with current time so that the numbers will be different every time
+    srand((unsigned int)(time(NULL)));
+
+    char numbers[] = "0123456789";
+    char letters[] = "abcdefghijklmnoqprstuvwyzx";
+    char LETTERS[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+    char *password = malloc(N+1);
+
+    for (i = 0; i < N; i++) {
+        randomizer = rand() % 3;
+        switch (randomizer) {
+            case 0:
+                password[i] = numbers[rand() % 10];
+                break;
+            case 1:
+                password[i] = letters[rand() % 26];
+                break;
+            case 2:
+                password[i] = LETTERS[rand() % 26];
+                break;
+        }
+    }
+
+    password[N] = '\0';
+    return password;
+}
+
+
+void createNewAccount()
+{
+//    gotoxy(2, 3);
+//    printf("Creating New Account:");
+//    gotoxy(2, 4);
+//    printf("-----------=---------");
+//
+//    printf("\nEnter your full name ");
+//    scanf("%s",acc.full_name);
+//    fflush(stdin);
+//    printf("\nEnter your full Address ");
+//    scanf("%s",acc.address);
+//
+//    fflush(stdin);   // clear standard input
+//    printf("\nEnter your age ");
+//    scanf("%d",&acc.age);
+//
+//    printf("\nEnter your National ID\n");
+//    scanf("%s",acc.national_id);
+//
+//    if (acc.age < 21){
+//        fflush(stdin);   //clear standard input
+//        printf("\nEnter your Guardian National ID ");
+//        scanf("%s",acc.guardian_nat_id);
+//    }
+//    printf("\nEnter your Balance ");
+//    scanf("%f",&acc.balance);
+//
+//    acc.password = randomPasswordGeneration(6);
+//    printf("Bank account Id created randomly\n");
+//    printf("Password created randomly: %s\n", acc.password);
+//
+//    stpcpy(acc.status,"Active");
+//    // create filename based on national id just for now
+//    stpcpy(filename, acc.national_id);  // todo change to bank account id
+//    fp=fopen(strcat(filename,".dat"),"w");
+//    fwrite(&acc,sizeof(acc),1,fp);
+//        if(fwrite != 0){
+//            printf("Registered Succesfully\n");
+//        }
 }
